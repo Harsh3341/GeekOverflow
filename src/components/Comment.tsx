f47dd50d9dc03e0dea4fb146f6a7ee90f713d477
Harsh <harshgupta3341@gmail.com>
2023-06-14 04:26:06 +0530

    added comment section

---
+++ src/components/Comment.tsx
@@ -0,0 +1,85 @@
+import {View, Text, Pressable, TextInput} from 'react-native';
+import React, {useContext, useState} from 'react';
+import {AppwriteContext} from '../appwrite/AppwriteContext';
+import {Comments} from './Post';
+
+const Comment = ({setComment}: any) => {
+  const [toggle, setToggle] = useState(false);
+  const [addComment, setAddComment] = useState<string>('');
+
+  const {user} = useContext(AppwriteContext);
+
+  const handleComment = () => {
+    const comment: Comments = {
+      name: user.name,
+      comment: addComment,
+      userId: user.$id,
+    };
+
+    setComment(comment);
+    setToggle(false);
+    setAddComment('');
+  };
+
+  return (
+    <View
+      style={{
+        backgroundColor: '#ffffff',
+        padding: 20,
+        marginTop: 20,
+        alignItems: 'center',
+      }}>
+      <Text
+        style={{
+          fontSize: 15,
+          color: '#000000',
+        }}
+        onPress={() => setToggle(!toggle)}>
+        add comment
+      </Text>
+      {toggle ? (
+        <View
+          style={{
+            width: '100%',
+            alignItems: 'flex-end',
+          }}>
+          <TextInput
+            style={{
+              width: '100%',
+              height: 100,
+              backgroundColor: '#a0a0a0',
+              marginTop: 20,
+              borderRadius: 5,
+              padding: 10,
+              verticalAlign: 'top',
+            }}
+            placeholder="Comment"
+            multiline={true}
+            numberOfLines={4}
+            onChangeText={text => setAddComment(text)}
+            value={addComment}
+          />
+          <Pressable
+            style={{
+              marginTop: 20,
+              backgroundColor: '#000000',
+              padding: 5,
+              borderRadius: 5,
+            }}
+            onPress={
+              addComment.length > 0 ? handleComment : () => setToggle(false)
+            }>
+            <Text
+              style={{
+                color: '#ffffff',
+              }}>
+              Add Comment
+            </Text>
+          </Pressable>
+        </View>
+      ) : null}
+    </View>
+  );
+};
+
+export default Comment;
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,13 +1,98 @@
 import {View, Text, ScrollView, Pressable} from 'react-native';
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {AppStackParamList} from '../routes/AppStack';
 import {Divider} from 'react-native-paper';
+import Comment from './Comment';
+import {AppwriteContext} from '../appwrite/AppwriteContext';
+import {Config} from '../utils/config';
+import {Permission, Query, Role} from 'appwrite';
+import {Subscribe} from '../appwrite/service';
 
 type PostProps = NativeStackScreenProps<AppStackParamList, 'Post'>;
 
+export type Comments = {
+  name: string;
+  comment: string;
+  userId: string;
+};
+
 const Post = ({navigation, route}: PostProps) => {
   const {question, profile}: any = route.params;
+
+  const {appwrite} = useContext(AppwriteContext);
+
+  const [comments, setComments] = useState<Comments[]>([]);
+
+  const setComment = (comment: Comments) => {
+    const data = {
+      databaseId: Config.APPWRITE_DATABASE_ID,
+      collectionId: Config.APPWRITE_COMMENTS_COLLECTION_ID,
+      data: {
+        name: comment.name,
+        comment: comment.comment,
+        userId: comment.userId,
+        questionId: question.$id,
+      },
+      permissions: [
+        Permission.read(Role.any()),
+        Permission.write(Role.user(comment.userId)),
+        Permission.delete(Role.user(comment.userId)),
+        Permission.update(Role.user(comment.userId)),
+      ],
+    };
+
+    appwrite
+      .createDocument(data)
+      .then((res: any) => {
+        if (res) {
+          setComments([...comments, comment]);
+        }
+      })
+      .catch(err => console.log(err));
+  };
+
+  useEffect(() => {
+    const data = {
+      databaseId: Config.APPWRITE_DATABASE_ID,
+      collectionId: Config.APPWRITE_COMMENTS_COLLECTION_ID,
+      queries: [Query.equal('questionId', question.$id)],
+    };
+    appwrite
+      .listDocuments(data)
+      .then((res: any) => {
+        if (res) {
+          setComments(res.documents);
+        }
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  // useEffect(() => {
+  //   const data: Subscribe = {
+  //     channel: ['collections.{collectionId}.documents'],
+  //     callback: data => {
+  //       if (data.event === 'database.documents.create') {
+  //         setComments([...comments, data.payload]);
+  //       }
+  //     },
+  //   };
+
+  //   const unsubscribe = appwrite
+  //     .subscribe(data)
+  //     .then((res: any) => {
+  //       console.log(res);
+  //     })
+  //     .catch(err => console.log(err));
+
+  //   return () => {
+  //     unsubscribe
+  //       .then((res: any) => {
+  //         console.log(res);
+  //       })
+  //       .catch(err => console.log(err));
+  //   };
+  // }, [comments]);
 
   return (
     <ScrollView
@@ -91,6 +176,38 @@ const Post = ({navigation, route}: PostProps) => {
             {question.tags}
           </Text>
         </View>
+        {comments.length > 0
+          ? comments.map((comment, index) => (
+              <View
+                key={index}
+                style={{
+                  width: '100%',
+                  alignItems: 'flex-start',
+                  marginTop: 20,
+                }}>
+                <Text
+                  style={{
+                    fontSize: 15,
+                    color: '#000000',
+                    fontWeight: 'bold',
+                  }}>
+                  {comment.name}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 15,
+                    color: '#000000',
+                  }}>
+                  {comment.comment}
+                </Text>
+
+                <Divider style={{width: '100%', marginVertical: 20}} />
+              </View>
+            ))
+          : null}
+      </View>
+      <View>
+        <Comment setComment={setComment} />
       </View>
     </ScrollView>
   );
